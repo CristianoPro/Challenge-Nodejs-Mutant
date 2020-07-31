1b3d72ed11471b7e13526350510570609675d033
Cristiano S Oliveira <cristiano.so@hotmail.com>
2020-07-31 17:38:53 -0300

    test: ensure ApiAdapter return is an Array

--- src/services/ApiAdapter.spec.ts
+++ src/services/ApiAdapter.spec.ts
@@ -12,10 +12,16 @@ const makeSut = (): ApiAdapter => {
 }
 
 describe('Api adapter', () => {
-  it('should call api with correct url', async () => {
+  it('Should call api with correct url', async () => {
     const sut = makeSut()
     const getSpy = jest.spyOn(axios, 'get')
     await sut.getUser(makeFakeRequest())
     expect(getSpy).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/users')
+  })
+
+  it('Should return an Array', async () => {
+    const sut = makeSut()
+    const users = await sut.getUser(makeFakeRequest())
+    expect(Array.isArray(users)).toBe(true)
   })
 })
