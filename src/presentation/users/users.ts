bbf9197e9f26349ed3ea3a52bb51334337178348
Cristiano S Oliveira <cristiano.so@hotmail.com>
2020-07-31 21:18:56 -0300

    feat: ensure UserController return 404 if not find users

---
+++ src/presentation/users/users.ts
@@ -0,0 +1,20 @@
+import { Controller } from '../protocols/controller'
+import { ApiAdapter } from '../../services/ApiAdapter'
+import { HttpResponse, HttpRequest } from '../protocols/http'
+
+export class UserController implements Controller {
+  private readonly api: ApiAdapter
+  constructor (api: ApiAdapter) {
+    this.api = api
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const users = await this.api.getUser(httpRequest)
+    if (!users) {
+      return {
+        statusCode: 404,
+        body: 'Not Found'
+      }
+    }
+  }
+}
